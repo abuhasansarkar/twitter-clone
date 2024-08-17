e98838ed6d662500a5931871db50a8e7bf5ae760
unknown <abuhasansarkar2@gmail.com>
2024-08-17 15:38:27 +0600

    Added Emoji in UI

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,6 +1,10 @@
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import NotificationPage from './components/NotificationPage';
+import ProfilePage from './components/ProfilePage';
+import RightPanel from './components/RightPanel';
+import Sidebar from './components/Sidebar';
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import Register from "./pages/Register";
@@ -8,12 +12,18 @@ import Register from "./pages/Register";
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* Sidebar */}
       <BrowserRouter basename="/">
+      <Sidebar/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/notifications" element={<NotificationPage />} />
+          <Route path="/profile/:username" element={<ProfilePage />} />
         </Routes>
+
+        <RightPanel/>
       </BrowserRouter>
       <Toaster position="top-right"/>
     </div>
